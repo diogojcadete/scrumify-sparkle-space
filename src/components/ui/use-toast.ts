8ed864839c4a6064ec6450cfd969d564541ebaf7
Cttouii <thecttouii@gmail.com>
2025-03-19 21:49:14 +0000

    del

--- src/components/ui/use-toast.ts
+++
@@ -1,6 +0,0 @@
-
-// We need to fix the double import issue to prevent duplicate toast notifications
-import { useToast } from "@/hooks/use-toast";
-import { toast } from "sonner";
-
-export { useToast, toast };
